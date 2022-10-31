64f6b249eeca404428fc1fb2bb6454e833d95b26
Raivals <103427644+Raivals@users.noreply.github.com>
2022-10-31 02:23:51 +0100

    Fin partie Projet

---
+++ src/components/Projects.js
@@ -0,0 +1,89 @@
+import { Col, Container, Nav, Row, Tab } from 'react-bootstrap'
+import { ProjectCard } from './ProjectCards'
+import colorSharp2 from '../assets/img/color-sharp2.png'
+import projectImg1 from '../assets/img/projectImg1.png'
+import projectImg2 from '../assets/img/projectImg2.png'
+import projectImg3 from '../assets/img/projectImg3.png'
+import projectImg4 from '../assets/img/projectImg4.png'
+import projectImg5 from '../assets/img/projectImg5.png'
+import projectImg6 from '../assets/img/projectImg6.png'
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: 'Booki',
+            description: 'Turn a mockup into a website with HTML & CSS',
+            imgUrl: projectImg1,
+        },
+        {
+            title: 'Ohmyfood',
+            description: 'Liven up a web page with CSS animations',
+            imgUrl: projectImg2,
+        },
+        {
+            title: 'La panthère agence',
+            description: 'Optimize an existing website',
+            imgUrl: projectImg3,
+        },
+        {
+            title: 'Kanap',
+            description: 'Build an e-commerce site in JavaScript',
+            imgUrl: projectImg4,
+        },
+        {
+            title: 'Piiquante',
+            description: 'Build a secure API for a food review app',
+            imgUrl: projectImg5,
+        },
+        {
+            title: 'Groupomania',
+            description: 'Create a corporate social network.',
+            imgUrl: projectImg6,
+        },
+    ]
+    return (
+        <section className="project" id="projects">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>
+                            Here are my projects. If you want to see more about
+                            them, you can check my Github
+                            <a href="https://github.com/Raivals"> here !</a>
+                        </p>
+                        <Tab.Container
+                            id="projects-tabs"
+                            defaultActiveKey="first"
+                        >
+                            <Nav
+                                variant="pills"
+                                className="nav-pills mb-5 justify-content-center align-items-center"
+                                id="pills-tab"
+                            ></Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {projects.map((project, index) => {
+                                            return (
+                                                <ProjectCard
+                                                    key={index}
+                                                    {...project}
+                                                />
+                                            )
+                                        })}
+                                    </Row>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img
+                className="background-image-right"
+                src={colorSharp2}
+                alt="background img right"
+            />
+        </section>
+    )
+}
